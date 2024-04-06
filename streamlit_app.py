0df0084ab5f9e1b2fe5a4b5664d761af1690d085
Kunning Shen <ks764@duke.edu>
2024-04-05 23:10:23 -0400

    init

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,11 @@
 import streamlit as st
 from transformers import pipeline
 
-# 设置标题
 st.title('English to Chinese')
-
-# 创建文本输入框，用户可以输入要翻译的英文
 text = st.text_area("Input your text in English:", value='', height=150, max_chars=None, key=None)
 
-# 加载Hugging Face模型，这里使用"Helsinki-NLP/opus-mt-en-zh"模型进行翻译
 translator = pipeline("translation_en_to_zh", model="Helsinki-NLP/opus-mt-en-zh")
 
-# 当用户输入文本并点击翻译按钮时进行翻译
 if st.button('Translate'):
     if text:
         # 执行翻译
